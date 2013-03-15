387ba642b8d456eb84af1195b75f468abbc8cf52
dlei <leidongming@gmail.com>
2013-03-15 09:56:18 -0500

    remove crispy form

--- wsgi/foodbase/settings.py
+++ wsgi/foodbase/settings.py
@@ -153,7 +153,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'users',
-    'crispy_forms',
+#    'crispy_forms',
 )
 
 # A sample logging configuration. The only tangible logging
