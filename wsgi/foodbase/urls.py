0e7e35b62e2013309a559394c7079a5ab7009a0c
dlei <leidongming@gmail.com>
2013-03-21 12:13:30 -0500

    add index page

--- wsgi/foodbase/urls.py
+++ wsgi/foodbase/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls')),
-    url(r'^/', include('users.urls')),
+    url(r'^$', include('users.urls')),
 
 
     
